a87d25cf7c2625d7a09cb84684a87de12550dfe7
TangaElLoko <martintapiaxavier@gmail.com>
2023-12-02 23:18:34 +0100

    Completed Step two!
    Now the user can set the size of the grid!!!

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const grid = document.querySelector('.grid');
-
-for (let y = 0; y < 16; y++) {
-    const newCol = document.createElement('div');
-    newCol.classList.add('col');
-
-    for(let x = 0; x < 16; x++) {
-        const newBlock = document.createElement('div');
-        newBlock.classList.add('block');
-        newBlock.addEventListener('mouseover', () => newBlock.classList.add('hovered'));
-        newCol.appendChild(newBlock);
+const createGrid = (size) => {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    for (let y = 0; y < size; y++) {
+        const newCol = document.createElement('div');
+        newCol.classList.add('col');
+        for(let x = 0; x < size; x++) {
+            const newBlock = document.createElement('div');
+            newBlock.style.cssText = `height: calc(80vh / ${size})`;
+            newBlock.classList.add('block');
+            newBlock.addEventListener('mouseover', () => newBlock.classList.add('hovered'));
+            newCol.appendChild(newBlock);
+        }
+        grid.appendChild(newCol);
     }
-    grid.appendChild(newCol);
+    document.body.appendChild(grid);
 }
+
+const sizeButton = document.querySelector('.inputButton');
+const sizeInput  = document.querySelector('#sizeInput');
+sizeButton.addEventListener('click', () => {
+    console.log(sizeInput.value);
+    if (parseInt(sizeInput.value) <= 100 && parseInt(sizeInput.value) > 0) {
+        const newSize = parseInt(sizeInput.value);
+        sizeInput.value = '';
+        sizeInput.focus();
+        document.body.removeChild(document.querySelector('.grid'));
+        createGrid(newSize);
+    } else {
+        alert('Size must be under 100 and a positive number!')
+    }
+});
+
+let actualSize = 16;
+createGrid(actualSize);
